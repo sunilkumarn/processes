ebf53579ed6211b09104fc4ae34f541416571432
sunil <sunilkumar.gec56@gmail.com>
2013-01-30 23:09:00 +0530

    Documenting the methods.

--- app/controllers/processes_controller.rb
+++ app/controllers/processes_controller.rb
@@ -1,9 +1,11 @@
 class ProcessesController < ApplicationController
 
+  # Lists the currently process ids
   def list
     render :json => `ps -o "pid"`.strip.split("\n")[1..-1]
   end
 
+  # Provides details on the process id supplied as the parameter
   def show
     pid = params[:id]
     process_details = `ps -o "pid pcpu pmem time comm" | grep #{pid}`
